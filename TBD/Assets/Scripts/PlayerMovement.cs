0a05448254c370d1ee6999a70c0edd74ca309e42
rtuomas <tuomas.a.rajala@gmail.com>
2019-09-28 19:51:27 +0300

    Player movement
    Pystyy liikkumaan edes takas sekä hyppäämään spacesta.

--- TBD/Assets/Scripts/PlayerMovement.cs
+++ TBD/Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float moveSpeed = 5f;
+
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+
     void Update()
     {
+        Jump();
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+        transform.position += movement * Time.deltaTime * moveSpeed;
+
+
+    }
+
+    void Jump()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, 5f), ForceMode2D.Impulse);
+        }
         
     }
 }
