e48b4f0cc7b0f357d3fab1a68a739d6af18e08a7
Joonas v2.0 <joonas.lehtoranta@live.fi>
2019-10-01 19:41:28 +0300

    MainMenu melkeen done
    Pitää enää lisää skriptiin vaan se buildattu game scene

---
+++ TBD/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(""); 
+        /*LoadScene("tähän tulee itse peli scene nimi tai numero build managerissä") 
+        mutta koska meillä on vielä tbd nimi ja ei olla buildattu niin jätän tyhjäksi.*/
+    }
+    public void QuitGame()
+    {
+        Debug.Log("Quit!");
+        Application.Quit();
+        //Universaali exit käsky
+    }
+}
