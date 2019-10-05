c5ab55a61d357c8cb732ea55dfc4271d8573e62e
Ville Riepponen <villeriepponen@gmail.com>
2019-10-05 16:07:56 +0300

    Aiming and shooting
    aiming and raycast shooting added

--- TBD/Assets/Scripts/PlayerController.cs
+++ TBD/Assets/Scripts/PlayerController.cs
@@ -7,16 +7,23 @@ public class PlayerController : MonoBehaviour
 
     public float moveSpeed;                 //Public variables with no declaration can be changed inside Unity Inspector
     public float jumpForce;
+    public float rSpeed;
 
-    public KeyCode left;                    //TODO: Assign each keycode for each player in Unity
+    public int health = 100;
+
+    private bool isFacingRight;
+
+    public KeyCode left;
     public KeyCode right;
     public KeyCode jump;
-    public KeyCode shoot;                   //TODO: Use "keycode shoot" to make players shoot projectiles
     public KeyCode aimUp;
     public KeyCode aimDown;
 
     private Rigidbody2D rb;
 
+    public GameObject player;
+
+    public Transform arms;
     public Transform groundCheckPoint;
     public float groundCheckRadius;
     public LayerMask whatIsGround;
@@ -27,6 +34,7 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        arms = this.gameObject.transform.GetChild(0);
     }
 
     // Update is called once per frame
@@ -53,14 +61,40 @@ public class PlayerController : MonoBehaviour
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
         }
 
-        //This flips the player so that the player is facing the way it is moving
+        //This flips the player and its children which is important for the raycast
 
-        if(rb.velocity.x < 0)
+        if(rb.velocity.x > 0 && isFacingRight)
         {
-            transform.localScale = new Vector3(-1, 1, 1);
-        } else if(rb.velocity.x > 0)
+            isFacingRight = !isFacingRight;
+            transform.Rotate(0, 180f, 0);
+        } else if(rb.velocity.x < 0 && !isFacingRight)
         {
-            transform.localScale = new Vector3(1, 1, 1);
+            isFacingRight = !isFacingRight;
+            transform.Rotate(0, 180f, 0);
         }
+
+        if (Input.GetKey(aimUp))
+        {
+            arms.transform.Rotate(new Vector3(0, 0, rSpeed) * Time.deltaTime);
+        }
+        else if (Input.GetKey(aimDown))
+        {
+            arms.transform.Rotate(new Vector3(0, 0, -rSpeed) * Time.deltaTime);
+        }
+    }
+
+    public void TakeDamage (int damage)
+    {
+        health -= damage;
+
+        if (health <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Destroy(gameObject);
     }
 }
---
+++ TBD/Assets/Scripts/Weapon.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public Transform firePoint;
+
+    public int damage;
+
+    public KeyCode shoot;                   //TODO: Use "keycode shoot" to make players shoot projectiles
+
+    public LineRenderer lineRenderer;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(shoot))
+        {
+            StartCoroutine(Shoot());
+        }
+    }
+
+    IEnumerator Shoot()
+    {
+        //Shooting logic
+        RaycastHit2D hitInfo = Physics2D.Raycast(firePoint.position, firePoint.right);
+
+        if (hitInfo)
+        {
+            PlayerController player = hitInfo.transform.GetComponent<PlayerController>();
+            if(player != null)
+            {
+                player.TakeDamage(damage);
+            }
+
+            lineRenderer.SetPosition(0, firePoint.position);
+            lineRenderer.SetPosition(1, hitInfo.point);
+        }
+        else
+        {
+            lineRenderer.SetPosition(0, firePoint.position);
+            lineRenderer.SetPosition(1, firePoint.position + firePoint.right * 100);
+        }
+
+        lineRenderer.enabled = true;
+
+        yield return new WaitForSeconds(0.02f);
+
+        lineRenderer.enabled = false;
+    }
+}
