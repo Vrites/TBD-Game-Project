0f8793f48aa83efafffbaf98211f607ae72180e4
Jopa <joonas.lehtoranta@live.fi>
2019-10-08 15:40:45 +0300

    Sounds finished

---
+++ TBD/Assets/Scripts/SetMusicVolume.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class SetMusicVolume : MonoBehaviour
+{
+    public AudioMixer mixer;
+    public Slider slider;
+
+    void Start()
+    {
+        slider.value = PlayerPrefs.GetFloat("BGMVolume", 0.75f);
+    }
+
+    public void SetLevel()
+    {
+        float sliderValue = slider.value;
+        mixer.SetFloat("BGMVolume", Mathf.Log10(sliderValue) * 20);
+        PlayerPrefs.SetFloat("BGMVolume", sliderValue);
+    }
+}
---
+++ TBD/Assets/Scripts/SetSFXVolume.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class SetSFXVolume : MonoBehaviour
+{
+    public AudioMixer mixer;
+
+    public Slider slider;
+
+    void Start()
+    {
+        slider.value = PlayerPrefs.GetFloat("SFXVolume", 0.75f); 
+    }
+
+    public void SetLevel()
+    {
+        float sliderValue = slider.value;
+        mixer.SetFloat("SFXVolume", Mathf.Log10(sliderValue) * 20);
+        PlayerPrefs.SetFloat("SFXVolume", sliderValue);
+    }
+}
